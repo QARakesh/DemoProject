ce2695212e4ca1bd30d195bb15db15abbf648549
QARakesh <rakesh_patra@mondaymorning.in>
2019-05-26 14:28:23 +0530

    comitting a .py file

---
+++ Newproject.py
@@ -0,0 +1,39 @@
+x=(1,2,3,4,5,6,7,8,9)
+count_odd=0
+count_even=0
+for i in x:
+    if i%2==0:
+        count_odd+=1
+    else:
+        count_even+=1
+print("Even number is :- ",count_even)
+print("Odd Number is :-",count_odd)
+
+n=5
+for i in range(n):
+    for j in range(i):
+        print(" * ",end=' ')
+    print(' ')
+for i in range(n,0,-1):
+    for j  in range(i):
+        print(" * ",end=' ')
+    print(' ')
+
+
+n=7
+for i in range(n,0,-1):
+    for j in range(i):
+        print(" # ",end=' ')
+    print(' ')
+for i in range(n+1):
+    for j in range(i):
+        print(" # ",end=' ')
+    print(' ')
+
+class election():
+    def __init__(self,country):
+        self.country=country
+
+
+
+
